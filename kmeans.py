cc249b683ade5875a6a0c9cff640bb28c62e786e
Camille Gillespie <gillec4@mcmaster.ca>
2025-02-14 18:16:23 +0100

    jupyter notebook added for kmeans applications much easier also added the loss function based on the ai labels

--- kmeans.py
+++ kmeans.py
@@ -4,6 +4,7 @@ import torch
 import csv
 import pandas as pd  
 from scipy import stats as st
+from sklearn.preprocessing import MinMaxScaler
 
 def kmeans_gpu(data, k, max_iters=100):
     data = data.to(device)
@@ -18,54 +19,103 @@ def kmeans_gpu(data, k, max_iters=100):
         for j in range(k):
             centroids[j] = data[clusters == j].mean(dim=0)
     
-    return clusters.cpu().numpy(), centroids
+    return clusters.cpu().numpy(), centroids.numpy()
 
 device = "cuda" if torch.cuda.is_available() else "cpu" 
 
-df = pd.read_csv('500_events_noiseless.csv')
-lf = pd.read_csv('labels_500_events_noiseless.csv')
+def labelmaker(events, density, noise, filename = None, folder = None): 
+    '''creates labels based on my naming convention for different files, keeps it consistent and easy'''
+    if folder: 
+        folder = folder + '/'
 
-columns = ['x[px]', 'y[px]', 't[s]']
-xycolumns = ['x[px]', 'y[px]']
-data_array = torch.Tensor(np.array(df[xycolumns]))
-labels_array = torch.Tensor(np.array(lf['labels']))
+    if filename: 
+        datafile = str(filename) 
+    else: 
+        datafile = str(events) + 'ev_' + str(density) + 'dense_n' + str(noise)
+    
+    labelfile = 'labels_' + datafile + '.csv'
+    sourcefile = 'sources_' + datafile + '.csv'
+    ai_labelfile = datafile + '_results' + '.csv'
+    centroidfile = datafile + '_centroids' + '.csv'
+
+    if folder: 
+        datafile = folder + datafile 
+        centroidfile = folder + centroidfile 
+        ai_labelfile = folder + ai_labelfile 
+        labelfile = folder + labelfile 
+        sourcefile = folder + sourcefile 
+
+    return datafile, labelfile, sourcefile, ai_labelfile, centroidfile
+
+def readfiles(datafile, labelfile, sourcefile): 
+    '''data reader and simplifier for files that haven't been passed through the algorithm'''
+    columns = ['x[px]', 'y[px]', 't[s]']
+    
+    dataread = pd.read_csv(datafile) 
+    data = np.array(dataread[columns])
+    
+    labelread = pd.read_csv(labelfile)
+    labels = np.array(labelread['labels'])
+
+    sourceread = pd.read_csv(sourcefile) 
+    sources = np.array(sourceread[columns])
+
+    return data, labels, sources
+
+def readai(datafile, labelfile, sourcefile, ai_labelfile, centroidfile):
+    '''file reader and data simplifier for data thats been through the algorithm'''
+    columns = ['x[px]', 'y[px]', 't[s]']
+    
+    dataread = pd.read_csv(datafile) 
+    data = np.array(dataread[columns])
+    
+    labelread = pd.read_csv(labelfile)
+    labels = np.array(labelread['labels'])
+
+    sourceread = pd.read_csv(sourcefile) 
+    sources = np.array(sourceread[columns])
+
+    ai_labelsread = pd.read_csv(ai_labelfile)
+    ai_labels = np.array(ai_labelsread['labels'])
+
+    centroidread = pd.read_csv(centroidfile) 
+    centroids = np.array(centroidread[columns])
+
+    return data, labels, sources, ai_labels, centroids
+
+events = None 
+density = None 
+noise = None 
+filename = None 
+folder = None 
+
+datafile, labelfile, sourcefile, ai_labelfile, centroidfile = labelmaker(events, density, noise, filename, folder)
+# read out the files
+data, labels, sources = readfiles(datafile, labelfile, sourcefile)
+# establish feature range and transform data, can comment this out to turn off scaling, naming is the same
+scaler = MinMaxScaler(feature_range=(0,1))
+data = scaler.fit_transform(data)
+sources = scaler.fit_transform(sources)
+# turn data into a tensor 
+data_tensor = torch.Tensor(data)
+
 
 # 30 is average number of photons per event
 k = 500
-clusters, centroids = kmeans_gpu(data_array, k, max_iters=5000) # clusters is the array of labels, centroids is the list of estimated centroids
-centroids = centroids.numpy()
-clusterFile = '500_events_noiseless_results_xy.csv'
-with open(clusterFile, mode = 'w', newline='') as wfile: 
+max_iters = 500
+ai_labels, centroids = kmeans_gpu(data_tensor, k, max_iters)
+
+with open(ai_labelfile, mode = 'w', newline='') as wfile: 
     writer = csv.writer(wfile)
     writer.writerow(['labels'])
-    for item in clusters: 
+    for item in ai_labels: 
                 writer.writerow([item])
 
 columns = ['x[px]', 'y[px]', 't[s]']
-centroidsFile = '500_events_noiseless_centroids_xy.csv'
-with open(centroidsFile, mode = 'w', newline = '') as wfile: 
+with open(centroidfile, mode = 'w', newline = '') as wfile: 
     writer = csv.writer(wfile)
     writer.writerow(columns)
     writer.writerows(centroids)
-
-# # print(clusters)
-# print(centroids)
-# #print(data_array)
-
-# plt.scatter(data_array[:,0], data_array[:,1], c = clusters, cmap='magma', marker='+')
-# plt.scatter(centroids[:,0], centroids[:,1], c='red', marker='o', s=15)
-# plt.title("Kmeans clustering 500 events x-y only")
-# plt.xlabel("X coordinates")
-# plt.ylabel("Y coordinates")
-# plt.savefig('xy 1000 iterations k-means clustering 500 events.png')
-
-# plt.figure()
-# plt.scatter(data_array[:,2], data_array[:,0], c = clusters, cmap='magma', marker='+')
-# plt.scatter(centroids[:,2], centroids[:,0], c='red', marker='o', s=15)
-# plt.title('Kmeans clustering 500 noiseless events')
-# plt.xlabel('Time (s)')
-# plt.ylabel('X coordiantes')
-# plt.savefig('xy 1000 iterations k-means space vs time 500 events.png')
 
 def breaks(array): 
     '''This function takes an array, goes through it item by item, and returns the list of indices where the value changes'''
@@ -90,32 +140,5 @@ def sizes(indices):
 
     return gaps
 
-
-def loss(labels, clusters): 
-    '''data is the array of coordinates, labels is the array of true labels, clusters is the array of assigned labels. 
-    This algorithm will go through the list of real and assigned labels and compare cluster sizes to determine how accurate the 
-    clustering is.
-    It returns the array of error percentages as well as the average error'''
-
-    true_breaks = breaks(labels)
-    gaps = sizes(true_breaks)
-
-    errors = []
-    beginning = 0
-    for i in range(len(gaps)): 
-        end = true_breaks[i]
-        chunk = clusters[beginning:end]
-        mode = st.mode(chunk) # the label that appears most often is the label of the overall event (they won't always match between 
-                            # the model and real labels), this assumes that the algorithm can at least recognize an event and measures how 
-                            # homogeneous the labelling is 
-        appearances = np.count_nonzero(chunk == mode[0])
-        errors.append(1-(appearances/gaps[i]))
-        beginning = true_breaks[i]
-
-    return errors, np.average(errors)
-
-losses, avg = loss(labels_array, clusters)
-print('--------')
-print(f'Average loss: {avg}')
 
 
